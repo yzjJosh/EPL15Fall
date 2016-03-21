7fa6740ddd314ce56af8cec68bbc7cce57abc84e
Josh <yangzijiangjosh@gmail.com>
2016-03-21 09:18:11 -0500

    Added Josh

---
+++ ee380l/sp2016/students/zy2743/Project2a/Project2/Project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project2
+//
+//  Created by Josh on 2/25/16.
+//  Copyright © 2016 Josh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
