7e832924f29adf1c3defdd60cccc1e9ccd78fe78
Josh <yangzijiangjosh@gmail.com>
2016-04-07 13:46:19 -0500

    backup

--- ee380l/sp2016/students/zy2743/Project1c/Project1c/Project1c/Project1c/main.cpp
+++ ee380l/sp2016/students/zy2743/Project1c/Project1c/Project1c/Project1c/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <assert.h>
+#include <list>
 #include "../../../Vector.h"
 
 using std::cout;
 using std::endl;
+
+class Foo{
+public:
+    const int constructor;
+    
+    Foo(int& a): constructor(0){}
+    Foo(int& a, int& b): constructor(1){}
+    Foo(int&& a, int& b): constructor(2){}
+    Foo(int& a, int&& b, int&& c): constructor(3){}
+    Foo(int& a, int& b, int& c): constructor(4){}
+    Foo(int& a, int& b, int& c, int& d): constructor(5){}
+    Foo(int&& a, int&& b, int& c, int& d): constructor(6){}
+    Foo(Foo const&): constructor(7){}
+    Foo(Foo&& that): constructor(that.constructor){}
+};
 
 int main(void) {
     epl::vector<int> v;
@@ -13,22 +29,103 @@ int main(void) {
         assert(v[i] == i);
         assert(v.size() == i+1);
     }
+    try{
+        *(v.end()+1);
+        assert(false);
+    }catch(epl::invalid_iterator e){
+        assert(e.what() == "Severe");
+    }
+    auto begin = v.begin();
+    auto end = v.end();
+    for(int i=0; begin != end; begin++, i++)
+        assert(*begin == i);
+    epl::vector<int> copy(v.begin(), v.end());
+    begin = copy.begin();
+    end = copy.end();
+    for(int i=0; begin!=end; begin++, i++){
+        assert(*begin == copy[i]);
+        assert(v[i] == copy[i]);
+    }
+    std::list<int> l{0, 1, 2, 3, 4, 5, 6, 7};
+    copy = epl::vector<int>(l.begin(), l.end());
+    assert(copy.size() == l.size());
+    begin = copy.begin();
+    end = copy.end();
+    auto lbegin = l.begin();
+    for(int i=0; begin!=end; begin++, i++,lbegin++)
+        assert(*begin == *lbegin);
+    begin = v.begin();
     for(int i=0; i<10000; i++){
         v.push_front(i);
         assert(v[0] == i);
         assert(v.size() == 10001+i);
     }
+    try{
+        *begin;
+        assert(false);
+    }catch(epl::invalid_iterator e){
+        assert(e.what() == "Moderate");
+    }
+    begin = v.begin();
+    int value = v[v.size()-1];
+    v.pop_back();
+    v.emplace_back(value);
+    assert(v[v.size()-1] == value);
+    try{
+        *begin;
+        assert(false);
+    }catch(epl::invalid_iterator e){
+        assert(e.what() == "Mild");
+    }
+    begin = v.begin();
+    end = v.end();
+    for(int i=0; begin != end; begin++, i++)
+        assert(v[i] == *begin);
     for(int i=9999; i>=0; i--){
         assert(v[v.size()-1] == i);
         v.pop_back();
         assert(v.size() == 10000+i);
     }
+    begin = v.begin();
+    end = v.end();
+    for(int i=0; begin != end; begin++, i++)
+        assert(v[i] == *begin);
+    assert(*(v.begin()+10) == v[10]);
+    assert(*(v.end()-10) == v[v.size()-10]);
+    assert(v.end()-v.begin() == v.size());
+    assert(v.end()-epl::vector<int>(v).begin() == v.size());
+    begin = v.begin();
+    assert(*(begin++) == v[0]);
+    assert(*begin == v[1]);
+    assert(*(++begin) == v[2]);
+    assert(*begin == v[2]);
+    assert(*(begin--) == v[2]);
+    assert(*begin == v[1]);
+    assert(*(--begin) == v[0]);
+    assert(*begin == v[0]);
+    assert(*(begin += 10) == v[10]);
+    assert(*begin == v[10]);
+    assert(begin - v.begin() == 10);
+    assert(*(begin -= 5) == v[5]);
+    assert(*begin == v[5]);
+    assert(begin - v.begin() == 5);
+    assert(begin - 5 + v.size() == v.end());
+    assert(begin - v.begin() == 5);
+    assert(*begin++ == v[5]);
+    assert(*begin == v[6]);
+    assert(*begin-- == v[6]);
+    assert(*begin == v[5]);
+    assert(begin[0] == v[5]);
+    assert(begin[1] == v[6]);
+    assert(begin[-1] == v[4]);
+    assert(*(1+begin) == v[6]);
     for(int i=9999; i>=0; i--){
         assert(v[0] == i);
         v.pop_front();
         assert(v.size() == i);
     }
     assert(v.size() == 0);
+    assert(v.begin() == v.end());
     for(int i=0; i<10000; i++){
         v.push_back(std::move(i));
         assert(v[v.size()-1] == i);
@@ -36,7 +133,152 @@ int main(void) {
         assert(v[0] == i);
         assert(v.size() == (i+1)*2);
     }
+    begin = v.begin();
+    end = v.end();
+    for(int i=0; begin != end; begin++, i++)
+        assert(v[i] == *begin);
     while(v.size() > 0) v.pop_front();
+    begin = v.begin();
+    end = v.end();
+    for(int i=0; begin != end; begin++, i++)
+        assert(v[i] == *begin);
+    assert(v.size() == 0);
+    assert(v.begin() == v.end());
+    assert(v.begin() != epl::vector<int>(v).begin());
+    assert(!(v.begin() < epl::vector<int>(v).begin()));
+    assert(v.end()-epl::vector<int>(v).begin() == 0);
+    v = epl::vector<int>(6);
+    begin = v.begin();
+    v.push_back(0);
+    try{
+        *begin;
+        assert(false);
+    }catch(epl::invalid_iterator e){
+        assert(e.what() == "Moderate");
+    }
+    try{
+        begin + 10;
+        assert(false);
+    }catch(epl::invalid_iterator e){
+        assert(e.what() == "Moderate");
+    }
+    try{
+        begin - 10;
+        assert(false);
+    }catch(epl::invalid_iterator e){
+        assert(e.what() == "Moderate");
+    }
+    try{
+        begin ++;
+        assert(false);
+    }catch(epl::invalid_iterator e){
+        assert(e.what() == "Moderate");
+    }
+    try{
+        begin --;
+        assert(false);
+    }catch(epl::invalid_iterator e){
+        assert(e.what() == "Moderate");
+    }
+    try{
+        ++ begin;
+        assert(false);
+    }catch(epl::invalid_iterator e){
+        assert(e.what() == "Moderate");
+    }
+    try{
+        -- begin;
+        assert(false);
+    }catch(epl::invalid_iterator e){
+        assert(e.what() == "Moderate");
+    }
+    try{
+        begin += 10;
+        assert(false);
+    }catch(epl::invalid_iterator e){
+        assert(e.what() == "Moderate");
+    }
+    try{
+        begin -= 10;
+        assert(false);
+    }catch(epl::invalid_iterator e){
+        assert(e.what() == "Moderate");
+    }
+    try{
+        v.begin() == begin;
+        assert(false);
+    }catch(epl::invalid_iterator e){
+        assert(e.what() == "Moderate");
+    }
+    try{
+        v.begin() < begin;
+        assert(false);
+    }catch(epl::invalid_iterator e){
+        assert(e.what() == "Moderate");
+    }
+    try{
+        v.begin() - begin;
+        assert(false);
+    }catch(epl::invalid_iterator e){
+        assert(e.what() == "Moderate");
+    }
+    try{
+        begin = v.end() + 5;
+    }catch(epl::invalid_iterator e){
+        assert(false);
+    }
+    v = {1, 3, 5, 0 , 5};
+    assert(v.size() == 5);
+    assert(v.end() - v.begin() == v.size());
+    begin = v.begin();
+    end = v.end();
+    for(int i=0; begin != end; begin++, i++){
+        assert(v[i] == *begin);
+        *begin = -i;
+        assert(*begin == -i);
+        assert(v[i] == -i);
+    }
+    const epl::vector<int>& const_v = v;
+    epl::vector<int>::const_iterator const_begin = const_v.begin();
+    epl::vector<int>::const_iterator const_end = const_v.end();
+    assert(const_end-const_begin == const_v.size());
+    for(int i=0; const_begin != const_end; const_begin+=1, i++){
+        assert(v[i] == *const_begin);
+       // *const_begin = i;
+    }
+    const_begin = v.begin();
+    const_end = v.end();
+    for(int i=0; const_begin != const_end; const_begin+=1, i++)
+        assert(v[i] == *const_begin);
+    const_begin = const_v.begin();
+    assert(*(const_begin ++) == const_v[0]);
+    assert(*const_begin == const_v[1]);
+    assert(*(++ const_begin) == const_v[2]);
+    assert(*const_begin == const_v[2]);
+    assert(const_begin - const_v.begin() == 2);
+    assert(const_v.end() - const_v.begin() == const_v.size());
+    assert(*(const_begin--) == const_v[2]);
+    assert(*const_begin == const_v[1]);
+    assert(*(--const_begin) == const_v[0]);
+    assert(*const_begin == const_v[0]);
+    assert(*const_begin++ == v[0]);
+    assert(*const_begin == v[1]);
+    assert(*const_begin-- == v[1]);
+    assert(*const_begin == v[0]);
+    assert(*(const_begin += 1) == v[1]);
+    assert(const_begin[0] == v[1]);
+    assert(const_begin[1] == v[2]);
+    assert(const_begin[-1] == v[0]);
+    assert(*(1+const_begin) == v[2]);
+    const_begin = const_end - 1;
+    assert(const_begin - const_end == -1);
+    epl::vector<int>::const_iterator const_copy{const_begin-1};
+    assert(const_copy - const_begin == -1);
+//    auto c = const_copy + 1;
+//    c = begin;
+//    assert(c == const_copy+1);
+    //begin = const_begin;
+    v = {};
     assert(v.size() == 0);
     try{
         v[-1] = 0;
@@ -54,17 +296,45 @@ int main(void) {
         v.pop_front();
         assert(false);
     }catch(std::exception e){}
-    epl::vector<int> w{10000};
+    epl::vector<int> w(10000);
     assert(w.size() == 10000);
     for(int i=0; i<w.size(); i++){
         w[i] = i;
         assert(w[i] == i);
     }
+    epl::vector<int32_t> x(10);
+    auto B = x.begin();
+    auto E = x.end();
+    assert(B!=E);
+    assert(E-B == 10);
+    int32_t k = 0;
+    for (auto& v : x) {
+        v = k++;
+    }
+    const epl::vector<int32_t>& y = x;
+    int32_t s = 0;
+    for (const auto& v : y) {
+        s += v;
+    }
+    assert(s==45);
+    epl::vector<int32_t> X(1);
+    auto itr = X.begin();
+    X.pop_back();
+    try{
+        int a;
+        a = *itr;
+        assert(false);
+    } catch (epl::invalid_iterator ii){
+        assert(ii.level == epl::invalid_iterator::MILD);
+    }
     v = w;
     assert(v.size() == w.size());
+    assert(w.begin() != v.begin());
+    assert(w.end() != v.end());
+    assert(!(w.begin() < v.end()));
     for(int i=0; i<v.size(); i++)
         assert(v[i] == w[i]);
-    const epl::vector<int> p{v};
+    const epl::vector<int> p(v);
     assert(p.size() == v.size());
     for(int i=0; i<p.size(); i++)
         assert(p[i] == v[i]);
@@ -72,17 +342,77 @@ int main(void) {
     assert(w.size() == p.size());
     for(int i=0; i<w.size(); i++)
         assert(w[i] == p[i]);
-    epl::vector<int> q{std::move(w)};
+    epl::vector<int> q(std::move(w));
     assert(q.size() == p.size());
     for(int i=0; i<q.size(); i++)
         assert(q[i] == p[i]);
     q = q;
     for(int i=0; i<q.size(); i++)
         assert(q[i] == p[i]);
-    epl::vector<epl::vector<int>> vec{};
-    vec.push_front(epl::vector<int>{});
+    bool same = std::is_same<epl::vector<int>::iterator::iterator_category, std::random_access_iterator_tag>::value;
+    assert(same);
+    same = std::is_same<epl::vector<int>::iterator::value_type, int>::value;
+    assert(same);
+    same = std::is_same<epl::vector<int>::const_iterator::iterator_category, std::random_access_iterator_tag>::value;
+    assert(same);
+    same = std::is_same<epl::vector<int>::const_iterator::value_type, const int>::value;
+    assert(same);
+    epl::vector<epl::vector<int>> vec;
+    vec.push_front(epl::vector<int>());
     for(int i=0; i<100; i++)
         vec.push_back(vec[0]);
+    epl::vector<Foo> f;
+    int arg = 0;
+    f.emplace_back(arg);
+    assert(f.size() == 1);
+    assert(f[0].constructor == 0);
+    assert(f.begin()->constructor == 0);
+    assert(((const epl::vector<Foo>&)f).begin()->constructor == 0);
+    f.emplace_back(arg, arg);
+    assert(f.size() == 2);
+    assert(f[1].constructor == 1);
+    assert((f.begin()+1)->constructor == 1);
+    assert((((const epl::vector<Foo>&)f).begin()+1)->constructor == 1);
+    f.emplace_back(0, arg);
+    assert(f.size() == 3);
+    assert(f[2].constructor == 2);
+    assert((f.begin()+2)->constructor == 2);
+    assert((((const epl::vector<Foo>&)f).begin()+2)->constructor == 2);
+    f.emplace_back(arg, 0, 0);
+    assert(f.size() == 4);
+    assert(f[3].constructor == 3);
+    assert((f.begin()+3)->constructor == 3);
+    assert((((const epl::vector<Foo>&)f).begin()+3)->constructor == 3);
+    f.emplace_back(arg, arg, arg);
+    assert(f.size() == 5);
+    assert(f[4].constructor == 4);
+    assert((f.begin()+4)->constructor == 4);
+    assert((((const epl::vector<Foo>&)f).begin()+4)->constructor == 4);
+    f.emplace_back(arg, arg, arg, arg);
+    assert(f.size() == 6);
+    assert(f[5].constructor == 5);
+    assert((f.begin()+5)->constructor == 5);
+    assert((((const epl::vector<Foo>&)f).begin()+5)->constructor == 5);
+    f.emplace_back(0, 0, arg, arg);
+    assert(f.size() == 7);
+    assert(f[6].constructor == 6);
+    assert((f.begin()+6)->constructor == 6);
+    assert((((const epl::vector<Foo>&)f).begin()+6)->constructor == 6);
+    f.emplace_back(f[0]);
+    assert(f.size() == 8);
+    assert(f[7].constructor == 7);
+    assert((f.begin()+7)->constructor == 7);
+    assert((((const epl::vector<Foo>&)f).begin()+7)->constructor == 7);
+    f.emplace_back(f[0]);
+    assert(f.size() == 9);
+    assert(f[8].constructor == 7);
+    assert((f.begin()+8)->constructor == 7);
+    assert((((const epl::vector<Foo>&)f).begin()+8)->constructor == 7);
+    f.emplace_back(f[0]);
+    assert(f.size() == 10);
+    assert(f[9].constructor == 7);
+    assert((f.begin()+9)->constructor == 7);
+    assert((((const epl::vector<Foo>&)f).begin()+9)->constructor == 7);
     std::cout<<"pass!"<<std::endl;
     
     return 0;
--- ee380l/sp2016/students/zy2743/Project1c/Vector_PhaseC_unittests.cpp
+++ ee380l/sp2016/students/zy2743/Project1c/Vector_PhaseC_unittests.cpp
@@ -1,86 +1,239 @@
+/*
+ * epl::vector_PhaseC_unittests.cpp
+ * EE380L - Spring 2015
+ *
+ * Tests for epl::vector_PhaseC are organized into three sections: PhaseC, PhaseC1, and
+ * PhaseC2, corresponding to the C, C*, and C** requirements respectively. These tests
+ * are independent, so you may comment out the ones that you are not utilizing, and
+ * Google Test will run accordingly.
+ *
+ * These tests are not complete. Write additional tests on your own to test
+ * the rest of the functionality of your program. The tests used to grade your
+ * project will be more robust than those included in this file.
+ */
+
 #include <cstdint>
 #include <stdexcept>
 #include "gtest/gtest.h"
 #include "Vector.h"
 
-//define PHASE_C
+using std::begin;
+using std::end;
 
-using epl::Vector;
-using namespace std;
-
-#ifdef PHASE_C
-
-TEST(PhaseC, direct_init_list){
-	Vector<int> w{ 1, 2, 3 };
-	EXPECT_EQ(3, w.size());
+/*****************************************************************************************/
+// Phase C Tests
+/*****************************************************************************************/
+#if defined(PHASE_C0_0) | defined(PHASE_C)
+TEST (PhaseC, direct_init_list) {
+    epl::vector<int> w{ 0, 1, 2 };
+    EXPECT_EQ(3, w.size());
+    for (int i = 0; i < 3; ++i) {
+        EXPECT_EQ(i, w[i]);
+    }
 }
+#endif
 
-#define ARRAY_SIZE(X) (sizeof(X)/sizeof(*X))
-TEST(PhaseC, copy_init_list){
-  Vector<int> x = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 42, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int ans[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 42, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  EXPECT_EQ(ARRAY_SIZE(ans), x.size());
-  for(unsigned int i=0; i<ARRAY_SIZE(ans); ++i)
-    EXPECT_EQ(x[i], ans[i]);
+#if defined(PHASE_C0_1) | defined(PHASE_C)
+TEST (PhaseC, begin_vector) {
+    epl::vector<int> x{ 1, 2, 3 };
+    auto it = begin(x);
+    const auto& it2 = end(x);
+    //std::cout << *end(x) << std::endl;
+    EXPECT_EQ(1, *it);
+    EXPECT_TRUE(end(x) == it2);
 }
+#endif
 
-TEST(PhaseC, range_for_loop){
-	Vector<int32_t> x(10);
-
-	int32_t k = 0;
-	for (auto& v : x) {
-		v = k++;
-	}
-
-	const Vector<int32_t>& y = x;
-	int32_t s = 0;
-	for (const auto& v : y) {
-		s += v;
-	}
-
-	EXPECT_EQ(45, s);
+#if defined(PHASE_C0_2) | defined(PHASE_C)
+TEST(PhaseC, RandIteOperations){
+    epl::vector<int> x{1, 2, 3, 4};
+    epl::vector<int>::const_iterator ite1 = x.begin();
+    EXPECT_EQ(*ite1, 1);
+    EXPECT_EQ(ite1[3], 4);
+    auto ite2(ite1);
+    EXPECT_TRUE(ite1 == ite2);
+    ite2 = ite2 + 1;
+    EXPECT_TRUE(ite1 < ite2);
+    ite2 += 1;
+    EXPECT_TRUE(ite1 < ite2);
+    ite1++; ++ite1;
+    EXPECT_TRUE(ite1 == ite2);
+    EXPECT_EQ(*ite1, 3);
+    --ite1; ite1--;
+    EXPECT_EQ(ite2 - ite1, 2);
+    ite2 = ite2 - 1;
+    ite2 -= 1;
+    EXPECT_EQ(ite2 - ite1, 0);
 }
+#endif
 
-TEST(PhaseC, ItrExceptSevere){
-  Vector<int> x(1);
-  auto itr = begin(x);
-  x.pop_back();
-  try{
-    int a;
-    a = *itr;
-    FAIL();
-    *itr = a;
-  } catch (epl::invalid_iterator ii){
-    EXPECT_EQ(ii.level, epl::invalid_iterator::SEVERE);
-  }
+/*****************************************************************************************/
+// Phase C* Tests
+/*****************************************************************************************/
+#if defined(PHASE_C1_0) | defined(PHASE_C)
+TEST (PhaseC1, range_for_loop) {
+    epl::vector<int32_t> x(10);
+    
+    int32_t k = 0;
+    for (auto& v : x) {
+        v = k++;
+    }
+    
+    const epl::vector<int32_t>& y = x;
+    int32_t s = 0;
+    for (const auto& v : y)
+        s += v;
+    
+    EXPECT_EQ(45, s);
 }
+#endif
 
-TEST(PhaseC, ItrExceptModerate){
-	Vector<int>  x(3), y{1,2,3};
-	auto xi = begin(x);
-	
-	x = y;
-	try{
-		*xi = 5;
-    FAIL();
-	}
-	catch(epl::invalid_iterator ex){
-    EXPECT_EQ(ex.level, epl::invalid_iterator::MODERATE);
-	}
+#if defined(PHASE_C1_1) | defined(PHASE_C)
+TEST(PhaseC1, invalid_iterator) {
+    epl::vector<uint32_t> x(10);
+    auto it = x.begin();
+    x.pop_front();
+    try {
+        *it;
+        FAIL();
+    } catch (epl::invalid_iterator exc) {
+        SUCCEED();
+    } catch (...) {
+        FAIL();
+    }
 }
+#endif
 
-TEST(PhaseC, ItrExceptMild){
-  Vector<int> x(3);
-  auto itr = begin(x);
-  x.pop_back();
-  try{
-    volatile int a;
-    a = *itr;
-    FAIL();
-    *itr = a;
-  } catch (epl::invalid_iterator ii){
-    EXPECT_EQ(ii.level, epl::invalid_iterator::MILD);
-  }
+#if defined(PHASE_C1_2) | defined(PHASE_C)
+TEST(PhaseC1, RandIteConstructor){
+    epl::vector<int> x{1, 2, 3, 4};
+    epl::vector<int>::const_iterator ite1 = x.begin();
+    epl::vector<int>::const_iterator ite2 = x.begin();
+    ite1++; ite1++;
+    ite2 += 1;
+    
+    epl::vector<int> y(ite2, ite1);
+    
+    EXPECT_EQ(y[0], 2);
+    EXPECT_EQ(y.size(), 1);
+    EXPECT_THROW(y[1], std::out_of_range);
 }
+#endif
 
+/*****************************************************************************************/
+// Phase C** Tests
+/*****************************************************************************************/
+#if defined(PHASE_C2_0) | defined(PHASE_C)
+TEST (PhaseC2, ItrExceptSevere) {
+    epl::vector<int> x(1);
+    auto itr = x.begin();
+    x.pop_back();
+    try{
+        int a;
+        a = *itr;
+        FAIL();
+        *itr = a;
+    } catch (epl::invalid_iterator ii) {
+        EXPECT_EQ(ii.level, epl::invalid_iterator::SEVERE);
+    } catch (...) {
+        FAIL();
+    }
+}
+#endif
+
+#if defined(PHASE_C2_1) | defined(PHASE_C)
+TEST (PhaseC2, ItrExceptModerate) {
+    epl::vector<int> x(3), y{1,2,3};
+    auto xi = x.begin();
+    x = y;
+    try {
+        *xi = 5;
+        FAIL();
+    } catch (epl::invalid_iterator ex) {
+        EXPECT_EQ(ex.level, epl::invalid_iterator::MODERATE);
+    } catch (...) {
+        FAIL();
+    }
+}
+#endif
+
+#if defined(PHASE_C2_2) | defined(PHASE_C)
+TEST (PhaseC2, ItrExceptMild) {
+    epl::vector<int> x(3);
+    auto itr = x.begin();
+    x.pop_back();
+    try {
+        volatile int a;
+        a = *itr;
+        FAIL();
+        *itr = a;
+    } catch (epl::invalid_iterator ii) {
+        EXPECT_EQ(ii.level, epl::invalid_iterator::MILD);
+    } catch (...) {
+        FAIL();
+    }
+}
+#endif
+
+#if defined(PHASE_C2_3) | defined(PHASE_C)
+TEST (PhaseC2, ItrExceptMild2) {
+    
+    epl::vector<int> v {1, 2, 3};
+    auto ite = v.end();
+    v.pop_back();
+    try{
+        std::cout << *ite << std::endl;
+    } catch (epl::invalid_iterator ii) {
+        EXPECT_EQ(ii.level, epl::invalid_iterator::MILD);
+    } catch (...) {
+        FAIL();
+    }
+    
+}
+#endif
+
+#if defined(PHASE_C2_4) | defined(PHASE_C)
+TEST (PhaseC2, ItrExceptMild3) {
+    epl::vector<int> v {1, 2, 3};
+    auto ite = v.begin();
+    v.pop_front();
+    try{
+        std::cout << *ite << std::endl;
+    } catch (epl::invalid_iterator ii) {
+        EXPECT_EQ(ii.level, epl::invalid_iterator::MILD);
+    } catch (...) {
+        FAIL();
+    }
+    
+}
+#endif
+
+#if defined(PHASE_C2_5) | defined(PHASE_C)
+TEST (PhaseC2, ItrExceptSevere2) {
+    epl::vector<int> v {1, 2, 3};
+    auto ite = v.end() - 1;
+    v.pop_back();
+    try{
+        std::cout << *ite << std::endl;
+    } catch (epl::invalid_iterator ii) {
+        EXPECT_EQ(ii.level, epl::invalid_iterator::SEVERE);
+    } catch (...) {
+        FAIL();
+    }
+}
+#endif
+
+#if defined(PHASE_C2_6) | defined(PHASE_C)
+TEST (PhaseC2, ItrExceptSevere3) {
+    epl::vector<int> v {1}; // only one integer in the epl::vector
+    auto ite = v.begin();
+    v.pop_front();       // This line can do v.pop_back(). The result is the same.
+    try{
+        std::cout << *ite << std::endl;
+    } catch (epl::invalid_iterator ii) {
+        EXPECT_EQ(ii.level, epl::invalid_iterator::SEVERE);
+    } catch (...) {
+        FAIL();
+    }
+}
 #endif
